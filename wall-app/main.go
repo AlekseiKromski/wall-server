12aa7fec6550144cc38fdd4a2942d2241777dbab
alekseiKromski <aleksei.kromski@outlook.com>
2022-08-25 23:29:17 +0300

    Add core of application

---
+++ wall-app/main.go
@@ -0,0 +1,38 @@
+package wall_app
+
+import "time"
+
+type WallList struct {
+	wallList []*Record
+	limit    int
+	count    int
+}
+
+type Record struct {
+	date time.Time
+	text string
+}
+
+func CreateWallRecord(text string) *Record {
+	return &Record{text: text, date: time.Now()}
+}
+
+func CreateWallList(countOfRecords int) *WallList {
+	return &WallList{wallList: []*Record{}, limit: countOfRecords, count: 0}
+}
+
+func (wl *WallList) GetRecords() []*Record {
+	return wl.wallList
+}
+
+func (wl *WallList) PutRecord(r *Record) []*Record {
+	if wl.count >= wl.limit {
+		wl.wallList = append(wl.wallList[1:], r)
+		wl.count = 0
+	} else {
+		wl.wallList = append(wl.wallList, r)
+	}
+
+	wl.count++
+	return wl.wallList
+}
